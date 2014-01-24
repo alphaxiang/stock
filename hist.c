a7d83abf928997a97eaf2e894049679bb3c86c2d
alphaxiang <1907767981@qq.com>
2014-01-24 23:03:34 +0800

    Update hist.c

--- hist.c
+++ hist.c
@@ -10,28 +10,30 @@ void float_print( const char * str ,double x ,int newline)
 	
 	if(x < -9.995) 
 		 printf("%.2f  ",x) ;
-  else if(x > 9.995 )
-  	 printf(" %.2f  ",x);
-  else if(x < 0)
-  	 printf(" %.2f  ",x);
-  else 
-  	 printf("  %.2f  ",x);
+        else if(x > 9.995 )
+  	         printf(" %.2f  ",x);
+        else if(x < 0)
+  	         printf(" %.2f  ",x);
+        else 
+  	         printf("  %.2f  ",x);
   	 
-  if(newline == 1) printf("\n");
+        if(newline == 1) printf("\n");
 }
 
 void print_sat(char * name ,TRANSINFO sat)
 {
 	 printf("%d  " ,sat.record_time);
-   float_print("Close",sat.today_close ,0);  
+         float_print("Close",sat.today_close ,0);  
 	 float_print("Open_Change",100 * (sat.today_open - sat.last_close)/sat.last_close ,0);	 
 	 float_print("MAX_Change",100 * (sat.today_max - sat.last_close)/sat.last_close ,0);
 	 float_print("MIN_Change",100 * (sat.today_min - sat.last_close)/sat.last_close ,0);
 	 float_print("Today_Change",sat.today_change ,0); 	
+	 
 	 if(sat.today_close > sat.today_open)
 	 	  printf("R    ");
 	 else 
 	 	  printf("G    ");  
+	 	  
 	 printf("Transaction_mount %.2f\n" ,sat.transaction_mount); 
 }
 
@@ -48,25 +50,27 @@ int print_hist(char * name ,char *arg)
 	assert(tmp != NULL && fp != NULL);
 	
 	fseek(fp, 0, SEEK_END); 
-  length = ftell(fp);
+        length = ftell(fp);
   
-  n = length / sizeof(TRANSINFO) ;
-  n = n < MAX_RECORD ? n : MAX_RECORD ; 
+        n = length / sizeof(TRANSINFO) ;
+        n = n < MAX_RECORD ? n : MAX_RECORD ; 
   
-  printf("%s   Total %d Transaction record found\n" ,name,n);
+        printf("%s   Total %d Transaction record found\n" ,name,n);
   
-  if(strcmp(arg ,"-a") == 0 ) {
+        if(strcmp(arg ,"-a") == 0 ) {
   	
-     fseek(fp, 0, SEEK_SET); 
+           fseek(fp, 0, SEEK_SET); 
 	   fread(tmp ,sizeof(TRANSINFO) ,n ,fp) ;
+	   
 	   for(i = 0 ;i < n ;i++) {
               print_sat(name ,tmp[i]) ; 
               total_change += tmp[i].today_change ;
            }
-     printf("%s   :%d Day Total Change %.2f\n",name ,n ,total_change);
-  }
+           
+           printf("%s   :%d Day Total Change %.2f\n",name ,n ,total_change);
+        }
   
-  return n ;
+        return n ;
 }
 
 int main(int argc ,char **argv)
